498b4ee3dc79aa7937521e06f031104474cb7341
Wolf <56493038+CR7PT0W0LF@users.noreply.github.com>
2022-06-05 19:47:58 -0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,59 @@
+
+clear
+sudo apt install figlet -y
+clear
+figlet Server Maid
+echo "this might take a minute. go grab a redbull"
+sleep 3
+#Minecraft server install
+mkdir Minecraft
+cd Minecraft
+sudo apt update -y && sudo apt upgrade -y 
+clear
+#this is some security stuff to get rid of some programs that are a risk
+sudo apt-get --purge remove xinetd nis yp-tools tftpd atftpd tftpd-hpa telnetd rsh-server rsh-redone-server
+#normal utils for seeing system resources and changing config files
+ sudo apt install screen -y
+ sudo apt install atop -y 
+ sudo apt install htop -y
+ sudo apt install neovim -y
+ sudo apt install net-tools -y
+ sudo apt install speedtest-cli -y
+ sudo apt install curl -y 
+
+ clear
+  sudo mkdir Server
+  cd Server
+   sudo apt install openjdk-17-jre-headless
+   sudo wget https://github.com/ServerJars/updater/releases/download/v3.0.0/ServerJars-3.jar
+chmod +x ServerJars-3 
+    cd 
+    #This is a program that allows you TCP Tunnel your Servers so you don't have to port foward. Make sure to setup a accout 
+     mkdir TCP Tunnel
+     sudo wget https://playit.gg/downloads/playit-0.8.1-beta
+     sudo chmod +x playit-0.8.1-beta
+      cd 
+       clear
+     sudo mkdir Steam-Servers
+      cd Steam-Servers
+       sudo add-apt-repository multiverse
+ sudo dpkg --add-architecture i386
+ sudo apt update
+ sudo apt install lib32gcc-s1 steamcmd
+ sudo apt install steamcmd
+cd
+clear
+
+ figlet Complete
+ 
+sleep 5
+  sudo apt purge figlet -y 
+  clear
+
+
+
+
+     
+
+
+
